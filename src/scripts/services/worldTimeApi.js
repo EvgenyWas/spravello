d038ac22dca4d30dae59b4cb1133007ba087ffca
nikitalugovskih <91367793+nikitalugovskih@users.noreply.github.com>
2022-04-15 16:22:12 +0300

    fix: edit the name of the variable

--- src/scripts/services/worldTimeApi.js
+++ src/scripts/services/worldTimeApi.js
@@ -1,11 +1,11 @@
-function lastFetch() {
+function userTime() {
     (async function () {
         var data = await fetch('https://worldtimeapi.org/api/timezone/Europe/Minsk');
         const newFetch = await data.json();
         root.innerText = (newFetch.datetime.slice(11, -16))
     })()
 };
-lastFetch()
-setInterval(lastFetch, 1000)
+userTime()
+setInterval(userTime, 1000)
 
-export { lastFetch }
+export { userTime }
