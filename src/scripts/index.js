a8150832da7b2355965126e77db2d8bb8b9328bf
nikitalugovskih <91367793+nikitalugovskih@users.noreply.github.com>
2022-04-18 21:57:25 +0300

    fix: edit code style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,26 +2,20 @@ import { createElement } from './templates/templates.js'
 
 const generateModalTask = () => {
     const modalWindow = createElement("div", "modal__window");
-
-    const titleModal = createElement("input", "modal__title")
-
+    const titleModal = createElement("input", "modal__title");
     const modalDescription = createElement("input", "modal__description");
-
     const modalOptions = createElement("div", "modal__options");
-
     const selectModal = createElement("select", "modal__list", "Select User");
-
     const cancelBtn = createElement("button", "modal__cancel", "Сancel");
-
     const confirmBtn = createElement("confirm", "modal__confirm", "Confirm");
 
-titleModal.placeholder = "Title";
-modalDescription.placeholder = "Description";
-titleModal.dataset.type = "modalTitle";
-modalDescription.dataset.type = "descriptionModal";
-selectModal.dataset.type = "modalSelect";
-cancelBtn.dataset.type = "btnCancel";
-confirmBtn.dataset.type = "btnConfirm";
+    titleModal.placeholder = "Title";
+    modalDescription.placeholder = "Description";
+    titleModal.dataset.type = "modalTitle";
+    modalDescription.dataset.type = "descriptionModal";
+    selectModal.dataset.type = "modalSelect";
+    cancelBtn.dataset.type = "btnCancel";
+    confirmBtn.dataset.type = "btnConfirm";
 
-return modalWindow;
+    return modalWindow;
 }
