92b9080aa3c8431123208be8d78124aee48b0a15
Nastaaaia Shkulkova <askulkova4@gmail.com>
2022-04-21 21:24:39 +0300

    Correcting deficiences CT-11

--- src/scripts/components/changeCount.js
+++ src/scripts/components/changeCount.js
@@ -1,49 +1,11 @@
-let arrayOfTodos = [];
+const todobox = document.querySelector("#todo-box");
+const inprogresBox = document.querySelector("#inprogress-box");
+const doneBox = document.querySelector("#done-box");
 
-const todoCreation = function (
-  todoId,
-  todoTitle,
-  todoDesk,
-  todoUser,
-  todoTime,
-  isProgress
-) {
-  this.todoId = todoId;
-  this.todoTitle = todoTitle;
-  this.todoDesk = todoDesk;
-  this.todoUser = todoUser;
-  this.todoTime = todoTime;
-  this.isProgress = isProgress;
-};
-
-const one = new todoCreation(1, "hi", "Lorem", "Pavel", "20.04", "start");
-const two = new todoCreation(
-  2,
-  "hello",
-  "Lorem",
-  "John",
-  "20.04",
-  "inprogress"
-);
-const three = new todoCreation(3, "task", "Lorem", "Mark", "20.04", "start");
-const four = new todoCreation(4, "goal", "Lorem", "Katya", "20.04", "done");
-arrayOfTodos.push(one, two, three, four);
-
-function changeCount(node) {
-  const counterTodo = arrayOfTodos.filter(
-    (e) => e.isProgress === "start"
-  ).length;
-  node.innerText = `all Todos: ${counterTodo}`;
-};
-
-const Counter = function ({ todobox }) {
-   const todobox = document.querySelector("#todo-box");
-   const inprogresBox = document.querySelector("#inprogress-box");
-   const doneBox = document.querySelector("#done-box");
-
-  (this.todobox = todobox),
-    (this.inprogresBox = inprogresBox),
-    (this.doneBox = doneBox),
+const Counter = function ({ todobox, inprogresBox, doneBox }) {
+  this.todobox = todobox,
+    this.inprogresBox = inprogresBox,
+    this.doneBox = doneBox,
     (this.changeCount = function () {
       let count = arrayOfTodos.filter((e) => e.isProgress === "start").length;
       this.todobox.innerText = count;
