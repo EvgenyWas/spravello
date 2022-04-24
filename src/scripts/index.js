176ec771765d5f07b87e3b44de8bfca38ba1faae
Andrei Zhukavets <a.zhukovets@mail.ru>
2022-04-24 23:19:19 +0300

    Complete CT-18

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,15 @@ import { myTime } from "./services/worldTimeApi";
 import { getUsersFromApi } from "./services/getUsersFromApi";
 import { addBtn, generateModalTask, arrayOfTodos} from "./components/todo.js"
 import { generateTodo } from "./components/functionsForDom";
+import { LOCAL_STORAGE_API } from "./services/localStorageApi";
+import { changeCount } from "./components/changeCount";
+
+window.addEventListener("DOMContentLoaded", app);
+
+function app() {
+    LOCAL_STORAGE_API.addTasksFromStorageData();
+    changeCount();
+};
 
 // Launch time in header
 setInterval(myTime, 1000);
--- src/scripts/services/localStorageApi.js
+++ src/scripts/services/localStorageApi.js
@@ -1,6 +1,14 @@
+import { generateTodo } from "../components/functionsForDom";
+import { arrayOfTodos } from "../components/todo";
+
 const LOCAL_STORAGE_API = {
   key: {
     tasks: "tasks-data",
+  },
+  lists: {
+    todo: "start",
+    inprogress: "inProgress",
+    done: "done",
   },
   getStorageData() {
     let data = JSON.parse(localStorage.getItem(this.key.tasks));
@@ -8,6 +16,32 @@ const LOCAL_STORAGE_API = {
   },
   setStorageData(data) {
     localStorage.setItem(this.key.tasks, JSON.stringify(data));
+  },
+  addTasksFromStorageData() {
+    const listTodo = document.getElementById('todo-tasks');
+    const listInProgress = document.getElementById('inprogress-tasks');
+    const listDone = document.getElementById('done-tasks');
+    let storageData = this.getStorageData();
+    let tasksInTodo = storageData.filter(task => task.isProgress === 'start');
+    let tasksInInprogress = storageData.filter(task => task.isProgress === 'inProgress');
+    let tasksInDone = storageData.filter(task => task.isProgress === 'done');
+
+    const appendToList = (arr, node) => {
+      arr.forEach(task => {
+        node.append(generateTodo(
+          task.todoId,
+          task.todoTitle,
+          task.todoDesk,
+          task.todoUser,
+          task.todoTime,
+          task.isProgress));
+          arrayOfTodos.push(task);
+      });
+    };
+
+    appendToList(tasksInTodo, listTodo);
+    appendToList(tasksInInprogress, listInProgress);
+    appendToList(tasksInDone, listDone);
   },
 };
 
